46efe29bf5ca5d18ced2d4d949f932b6704a38df
Brad Bambara <brad.bambara@gmail.com>
2014-12-15 23:10:15 -0500

    initial commit

---
+++ AutoLayoutTricks/Controllers/AutoLayoutController.swift
@@ -0,0 +1,52 @@
+//
+//  AutoLayoutController.swift
+//  AutoLayoutTricks
+//
+//  Created by Brad Bambara on 9/30/14.
+//  Copyright (c) 2014 Brad Bambara. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutController: UIViewController {
+
+	//MARK: constraint resizing
+	
+	@IBOutlet weak var constraint1: NSLayoutConstraint!
+	@IBOutlet weak var constraint2: NSLayoutConstraint!
+	
+	@IBAction func slider1Changed(sender: AnyObject) {
+		
+		constraint1.constant = (CGFloat)((sender as UISlider).value)
+	}
+	
+	@IBAction func slider2Changed(sender: AnyObject) {
+		
+		constraint2.constant = (CGFloat)((sender as UISlider).value)
+	}
+	
+	//MARK: view remove
+	
+	@IBOutlet weak var removableView: UIView!
+
+	@IBAction func removeView(sender: AnyObject) {
+		
+		if let view = removableView {
+			view.removeFromSuperview()
+		}
+	}
+	
+	//MARK: animating constraints
+	
+	@IBAction func animateViews(sender: AnyObject) {
+		
+		constraint1.constant = CGFloat(arc4random_uniform(200))
+		constraint2.constant = CGFloat(arc4random_uniform(200))
+		
+		UIView.animateWithDuration(0.5, animations: { () -> Void in
+			self.view.setNeedsUpdateConstraints()
+			self.view.layoutIfNeeded()
+		})
+	}
+	
+}
